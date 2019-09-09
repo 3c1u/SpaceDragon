7ec13e566eaad5102c555c86a1fc1ef421b1840d
drumath <2237k.drumath@gmail.com>
2019-09-09 18:19:26 +0900

    PlayerViewでシューティングアクションの実装と疑似SoundDetectの実装(途中)

--- CyberAgentB/Assets/Scripts/ProtoType/tutumi_view/PlayerView.cs
+++ CyberAgentB/Assets/Scripts/ProtoType/tutumi_view/PlayerView.cs
@@ -2,8 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using ProtoType;
+
 public class PlayerView : MonoBehaviour
 {
+    public GameObject Bullet;
+    public GameObject Fire;
+
+    Vector3 Pos;
+    Quaternion Rot;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,23 @@ public class PlayerView : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(GameController.Instance.Player.Voice.isActive);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (GameController.Instance.Player.Voice.isActive)
+            {
+                LoanchBullet(GameController.Instance.Player.Voice.Power);
+            }
+            else if(GameController.Instance.Player.Breath.isActive)
+            {
+
+            }
+        }
+    }
+
+    void LoanchBullet(float Power)
+    {
+        var _bullet = Instantiate(Bullet);
+        _bullet.transform.localScale = new Vector3(1, 1, 1) * Power;
     }
 }
---
+++ CyberAgentB/Assets/Scripts/ProtoType/tutumi_view/SoundDetectProtoType.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ProtoType;
+
+public class SoundDetectProtoType : MonoBehaviour
+{
+    [SerializeField] bool isBreath;
+
+    [Header("Breath Data")]
+    [SerializeField] float breathPower;
+
+    [Header("Voice Data")]
+    [SerializeField] float voicePower;
+    [SerializeField] float voicePitch;
+
+    private void Update()
+    {
+        GameController.Instance.Player.Voice.isActive = !isBreath;
+        GameController.Instance.Player.Breath.isActive = isBreath;
+
+        GameController.Instance.Player.Voice.Power = voicePower;
+        GameController.Instance.Player.Voice.Pitch = voicePitch;
+
+        GameController.Instance.Player.Breath.Power = breathPower;
+    }
+}
