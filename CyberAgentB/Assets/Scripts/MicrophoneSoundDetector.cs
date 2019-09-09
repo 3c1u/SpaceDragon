2be05c191d6b4ac7f3382ea139d4a90a4a0f6715
3c1u (Hikaru Terazono) <298748+3c1u@users.noreply.github.com>
2019-09-10 01:08:33 +0900

    リファクタリング

--- CyberAgentB/Assets/Scripts/MicrophoneSoundDetector.cs
+++ CyberAgentB/Assets/Scripts/MicrophoneSoundDetector.cs
@@ -1,64 +1,103 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MicrophoneSoundDetector : MonoBehaviour {
-  [SerializeField] private AudioSource audioSource;
-  private Microphone _microphone;
+	[SerializeField] private AudioSource audioSource;
+	private int _samplingRate = 44100;
 
-  private const int FFT_SIZE = 1024;
-  private float[] _fftNoiseReductionMask = new float[FFT_SIZE];
-  private float[] _fftBlowingLowpassFilter = new float[FFT_SIZE];
-  private float[] _fftTonalBandpassFilter = new float[FFT_SIZE];
-  
-  public enum VoiceInputState {
-    /// <summary>
-    /// 音程、風のいずれも検知できない状態。
-    /// </summary>
-    Off = 0,
-    /// <summary>
-    /// 音程。
-    /// </summary>
-    Tonal,
-    /// <summary>
-    /// 風。
-    /// </summary>
-    Blow,
-  }
+	private const int FFT_SIZE = 1024;
+	private SimpleDetectionStrategy _strategy = new SimpleDetectionStrategy();
 
-  void Start() {
-    // マイクの初期化処理とかをやる
-    // let listener = MicrophoneListener.Listen().await?;
-    
-    // フィルタの生成
-    GenerateFilters();
-  }
-  
-  void Update() {
-    float freq;
-    var state = Process(audioSource, out freq);
-  }
+	private VoiceInputState _currentState = VoiceInputState.Off;
 
-  /// <summary>
-  /// 音声検出用のフィルターを初期化。
-  /// </summary>
-  void GenerateFilters() {
-    
-  }
+	public enum VoiceInputState {
+		/// <summary>
+		/// 音程、風のいずれも検知できない状態。
+		/// </summary>
+		Off = 0,
+		/// <summary>
+		/// 音程。
+		/// </summary>
+		Tonal,
+		/// <summary>
+		/// 風。
+		/// </summary>
+		Blow,
+	}
 
-  /// <summary>
-  /// オーディオソースの入力に対し、音声を判別して更新されたステートを返す。
-  /// </summary>
-  /// <param name="source">オーディオソース</param>
-  /// <param name="frequency">周波数の出力（検出された場合）</param>
-  /// <returns></returns>
-  VoiceInputState Process(AudioSource source, out float frequency) {
-    // FFTの結果を取り出す。
-    var fft_res = new float[FFT_SIZE];
-    source.GetSpectrumData(fft_res, 0, FFTWindow.Blackman);
+	IEnumerator Start() {
+		// マイクの取得
+		yield return ObtainMicrophone();
 
-    // 検出できなかったときの返り値
-    frequency = 0f;
-    return VoiceInputState.Off;
-  }
+		// TODO: ディテクターストラテジーの初期化が必要なら、ここでやる。　
+	}
+
+	IEnumerator ObtainMicrophone() {
+		int samplingRate = 44100;
+
+		// マイクの権限取得をする。
+		yield return Application.RequestUserAuthorization(UserAuthorization.Microphone);
+
+		if (Application.HasUserAuthorization(UserAuthorization.Microphone)
+			&& Microphone.devices.Length > 0) {
+			// マイクの権限を持っているなら、マイクを取得する
+
+			// マイクの能力を取得
+			int minSamplingFreq, maxSamplingFreq;
+			Microphone.GetDeviceCaps("", out minSamplingFreq, out maxSamplingFreq);
+
+			// 特殊なサンプリングレートを要求されたときはとりあえず最低4000Hzはあるように保証したい（声検出のため）
+			if (maxSamplingFreq != 0
+				&& (samplingRate < minSamplingFreq || maxSamplingFreq < samplingRate)) {
+				Debug.LogWarning("特殊なサンプリングレートの可能性があります： (Minimum: "
+									+ minSamplingFreq + " Hz, Maximum: " + maxSamplingFreq + " Hz)");
+				samplingRate = Math.Max(Math.Min(maxSamplingFreq, 4000), minSamplingFreq);
+			}
+
+			if (samplingRate < 4000)
+				Debug.LogWarning("サンプリングレートが4000Hz以下です。音声の検出に失敗するかもしれません。");
+
+			audioSource.clip = Microphone.Start("", true, 1, samplingRate);
+			audioSource.loop = true;
+
+			audioSource.Play();
+		} else {
+			Debug.LogError("マイクの取得に失敗しました。");
+		}
+
+		_samplingRate = samplingRate;
+	}
+
+	void Update() {
+		var state = Detect(audioSource, out _);
+
+		if (_currentState != state) {
+			_currentState = state;
+			Debug.Log(state);
+		}
+	}
+
+	void OnAudioFilterRead(float[] data, int channels) {
+		// TODO: 高速なFFTをするならコレを使うことになる？
+	}
+
+	/// <summary>
+	/// オーディオソースの入力に対し、音声を判別して更新されたステートを返す。
+	/// </summary>
+	/// <param name="source">オーディオソース</param>
+	/// <param name="frequency">周波数の出力（検出された場合）</param>
+	/// <returns></returns>
+	VoiceInputState Detect(AudioSource source, out float frequency) {
+		// FFTの結果を取り出す。（ローレイテンシーで動かすにはNativeの力が必要...?）
+
+		var fft_res = new float[FFT_SIZE];
+		source.GetSpectrumData(fft_res, 0, FFTWindow.Blackman);
+
+		// TODO: 周波数の取得
+		frequency = 0f;
+
+		return _strategy.Detect(fft_res, 44100);
+	}
 }
---
+++ CyberAgentB/Assets/Scripts/SimpleDetectionStrategy.cs
@@ -0,0 +1,40 @@
+﻿using static MicrophoneSoundDetector;
+
+public class SimpleDetectionStrategy : DetectorStrategy {
+	override public VoiceInputState Detect(float[] fft, int samplingRate) {
+		// マジックナンバー類。ウマくいかなければこの辺を弄るといいかも。
+		const float lowpass1 = 147.0f;
+		const float lowpass2 = 2000.0f;
+		const float lowThreshold = 0.0267f;
+		const float highThreshold = 0.064f;
+
+
+		float upperSum = 0f;
+		float lowerSum = 0f;
+
+		for (int i = 0; i < fft.Length; i++) {
+			float targetFreq = ((float) i) / fft.Length * samplingRate;
+
+			//	ローパス処理
+			if (targetFreq > lowpass2)
+				break;
+
+			if (targetFreq < lowpass1) {
+				lowerSum += fft[i];
+			} else {
+				upperSum += fft[i];
+			}
+		}
+
+		bool lowActivated = lowThreshold <= lowerSum;
+		bool highActivated = highThreshold <= upperSum;
+
+		// 入力のからの結果に基づいて場合分け
+		if (lowActivated) // (lowActivated && !highActivated) || (lowActivated && highActivated)
+			return VoiceInputState.Blow;
+		else if (highActivated) // !lowActivated
+			return VoiceInputState.Tonal;
+		else
+			return VoiceInputState.Off;
+	}
+}
