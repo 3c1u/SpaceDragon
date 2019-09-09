052548cf95f097bf80424f2a3e20adb0ecdd38cc
3c1u <298748+3c1u@users.noreply.github.com>
2019-09-09 19:22:30 +0900

    thresholdデバッグ機構

--- CyberAgentB/Assets/Scripts/Sound.cs
+++ CyberAgentB/Assets/Scripts/Sound.cs
@@ -13,6 +13,19 @@ public class Sound : MonoBehaviour
 
     bool ok = false;
 
+    private AudioSource m_audioSource;
+
+    [SerializeField]
+    private Slider frequencyBoundarySlider;
+
+    [SerializeField]
+    private Slider upperThreshold;
+
+    [SerializeField]
+    private Slider lowerThreshold;
+
+    [SerializeField]
+    private Text statusField;
 
     void Start()
     {
@@ -23,6 +36,8 @@ public class Sound : MonoBehaviour
         {
 
             AudioSource aud = GetComponent<AudioSource>();
+            m_audioSource = aud;
+
             if ((aud != null) && (Microphone.devices.Length > 0)) // オーディオソースとマイクがある
             {
                 Debug.Log("okがtrue");
@@ -30,14 +45,18 @@ public class Sound : MonoBehaviour
                 string devName = Microphone.devices[0]; // 複数見つかってもとりあえず0番目のマイクを使用
                 int minFreq, maxFreq;
                 Microphone.GetDeviceCaps(devName, out minFreq, out maxFreq); // 最大最小サンプリング数を得る
-                aud.clip = Microphone.Start(devName, true, 2, 44100/*minFreq*/); // 音の大きさを取るだけなので最小サンプリングで十分
+                aud.clip = Microphone.Start(devName, true, 1, 44100/*minFreq*/); // 音の大きさを取るだけなので最小サンプリングで十分
                 aud.Play(); //マイクをオーディオソースとして実行(Play)開始
                 ok = true;
                 Debug.Log("変わった" + ok);
-            }else{
+            }
+            else
+            {
                 Debug.Log("マイク不備");
             }
-        }else{
+        }
+        else
+        {
             volumetext.text = "許可降りてない";
         }
 
@@ -47,15 +66,35 @@ public class Sound : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(m_volumeRate);
-        if (ok)
+        var spectrum = new float[1024];
+
+        m_audioSource.GetSpectrumData(spectrum, 0, FFTWindow.Blackman);
+
+        var upper = 0f;
+        var lower = 0f;
+
+        var frequencyBoundary = frequencyBoundarySlider.value;
+
+        for (int i = 0; i < spectrum.Length; i++)
         {
-            volumetext.text = m_volumeRate + "";
+            var targetFrequency = i * 22050.0 / spectrum.Length;
+            if (frequencyBoundary < targetFrequency)
+            {
+                upper += spectrum[i];
+            }
+            else
+            {
+                lower += spectrum[1];
+            }
         }
+
+        statusField.text = frequencyBoundary + " Hz, lower: " + lowerThreshold.value
+                            + " (" + (lower > lowerThreshold.value) + "), higher: "
+                            + upperThreshold.value + " (" + (upper > upperThreshold.value) + ")";
     }
 
     // オーディオが読まれるたびに実行される
-    private void OnAudioFilterRead(float[] data, int channels)
+    /* private void OnAudioFilterRead(float[] data, int channels)
     {
         float sum = 0f;
         for (int i = 0; i < data.Length; ++i)
@@ -63,8 +102,8 @@ public class Sound : MonoBehaviour
             sum += Mathf.Abs(data[i]); // データ（波形）の絶対値を足す
         }
         // データ数で割ったものに倍率をかけて音量とする
-        m_volumeRate = Mathf.Clamp01(sum * m_gain / (float)data.Length)*10000;
-    }
+        m_volumeRate = Mathf.Clamp01(sum * m_gain / (float)data.Length) * 10000;
+    } *?
 
     /*[SerializeField] Text volumetext;
 
