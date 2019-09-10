2480d0dd7b1312fe619e6095f0755f4cecc40e2b
江口　大喜 <daikiguitar.gbs@docomo.ne.jp>
2019-09-10 21:39:35 +0900

    敵の生成

--- CyberAgentB/Assets/Scripts/ProtoType/FireView.cs
+++ CyberAgentB/Assets/Scripts/ProtoType/FireView.cs
@@ -5,8 +5,52 @@ using UnityEngine;
 
 public class FireView : MonoBehaviour
 {
+
+    IceManager iceManager;
+
+    float middleHP;
+
+    bool once;
+
+    private void Start()
+    {
+        once = true;
+    }
+
     private void OnParticleCollision(GameObject other)
     {
         GameController.Instance.AddScore(1);
+
+
+        Debug.Log("氷");
+        if (other.gameObject.tag == "Ice")
+        {
+            if (once)
+            {
+                Debug.Log("氷衝突している");
+
+                iceManager = other.gameObject.GetComponent<IceManager>();
+
+                Debug.Log(iceManager);
+
+                if (GameController.Instance.Player.Breath.isActive && iceManager)
+                {
+                    middleHP = iceManager.HP - GameController.Instance.Player.Breath.Power;
+
+                    iceManager.HP = middleHP;
+                }
+
+                once = false;
+                Debug.Log(middleHP);
+                if (middleHP <= 0)
+                {
+                    Destroy(other.gameObject);
+                }
+                Destroy(this.gameObject);
+            }
+
+
+        }
     }
+
 }
