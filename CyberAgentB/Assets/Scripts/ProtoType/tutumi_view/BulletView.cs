d6149e8cfcfc27744ec300c5f00db7d1bd47c0a0
江口　大喜 <daikiguitar.gbs@docomo.ne.jp>
2019-09-11 01:37:05 +0900

    さらなる微調整。FireとBulletの判断

--- CyberAgentB/Assets/Scripts/ProtoType/tutumi_view/BulletView.cs
+++ CyberAgentB/Assets/Scripts/ProtoType/tutumi_view/BulletView.cs
@@ -10,10 +10,14 @@ public class BulletView : MonoBehaviour
 
     RockManager rockManager;
 
-    float middleHP;
+    //float middleHP;
     Vector3 forwardVec;
 
     bool once;
+
+    private IceManager _iceManager;
+    private RockManager _rockManager;
+    private int middleHP;
 
     // Start is called before the first frame update
     void Start()
@@ -32,7 +36,7 @@ public class BulletView : MonoBehaviour
         this.transform.position += speed * forwardVec;
     }
 
-    private void OnTriggerEnter(Collider other)
+    /*private void OnTriggerEnter(Collider other)
     {
 
         if (other.gameObject.tag == "Rock")
@@ -60,6 +64,71 @@ public class BulletView : MonoBehaviour
                 }
                 Destroy(this.gameObject);
             }
+        }
+    }*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Rock")
+        {
+            Debug.Log("Rock");
+            HitTarget(other.gameObject);
+        }
+    }
+
+    /*private void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.tag == "Rock")
+        {
+            Debug.Log("Rock");
+            HitTarget(other.gameObject);
+        }
+    }*/
+
+
+
+    private void HitTarget(GameObject other)
+    {
+
+
+        if (other.GetComponent<RockManager>())
+        {
+            _rockManager = other.gameObject.GetComponent<RockManager>();
+
+
+
+
+            if (GameController.Instance.Player.Voice.isActive && _rockManager)
+            {
+                middleHP = _rockManager.HP - (int)GameController.Instance.Player.Breath.Power * 10;
+                _rockManager.HP = (int)middleHP;
+            }
+
+            once = false;
+            Debug.Log(middleHP);
+            if (middleHP <= 0)
+            {
+                //Debug.Log("1");
+                DestroyEnemy(other);
+                Destroy(other.gameObject);
+            }
+        }
+    }
+
+    private void DestroyEnemy(GameObject other)
+    {
+        //Debug.Log("2");
+        if (other.GetComponent<IceManager>())
+        {
+            GameController.Instance.AddScore(IceManager.Point);
+            //Debug.Log("氷かさん");
+        }
+        if (other.GetComponent<RockManager>())
+        {
+            GameController.Instance.AddScore(RockManager.Point);
+
+            //Debug.Log("イワかさん");
         }
     }
 
